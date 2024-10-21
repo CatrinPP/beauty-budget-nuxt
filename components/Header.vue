fe9f5f254028ca17c3de02ceec85a799f17a2ff7
Caterina <e.parshina@allfuneral.com>
2024-10-21 10:48:05 +0300

    feat: add pages routing

--- components/Header.vue
+++ components/Header.vue
@@ -4,20 +4,27 @@
 
 <template>
   <header class="header" >
-    <button class="header__button" type="button">Настройки</button>
+    <HeaderLink to="/" title="Главная" icon="home" />
+    <nav class="header__nav">
+      <HeaderLink to="/history" title="История транзакций" icon="history" />
+      <HeaderLink to="/dashboard" title="Графики" icon="chart" />
+      <HeaderLink to="/settings" title="Настрокйки" icon="cog" />
+    </nav>
   </header>
 </template>
 
 <style scoped>
-.header {
-  display: grid;
-  gap: 20px;
-  grid-auto-flow: column;
-}
+  .header {
+    display: grid;
+    gap: 20px;
+    grid-auto-flow: column;
+    align-items: center;
+    justify-content: space-between;
+  }
 
-.header__button {
-  border: none;
-  margin: 0;
-  padding: 20px;
-}
+  .header__nav {
+    display: grid;
+    gap: 20px;
+    grid-auto-flow: column;
+  }
 </style>
---
+++ components/HeaderLink.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  interface Props {
+    to: string;
+    title: string;
+    icon: string;
+  }
+
+  const { to, title, icon } = defineProps<Props>();
+</script>
+
+<template>
+  <NuxtLink class="header-link" activeClass="header-link_active" :to :title>
+    <svg class="header-link__icon" :title viewBox="-2 0 44 44" width="44" height="44">
+      <use :href="`/sprite.svg#${icon}`"></use>
+    </svg>
+  </NuxtLink>
+</template>
+
+<style scoped>
+  .header-link {
+    display: inline-flex;
+    align-items: center;
+    box-sizing: border-box;
+    justify-content: center;
+
+    text-decoration: none;
+  }
+
+  .header-link_active {
+    .header-link__icon {
+      fill: red;
+    }
+  }
+
+  .header-link__icon {
+    fill: #000000;
+  }
+</style>
